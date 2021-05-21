e08ae4534239e70c362e21a6f7a33df31d9a2e81
Mikhail Lukianchenko <ml@akabos.com>
2021-05-21 12:25:22 +0300

    Add simple handler test

---
+++ handler_simple_test.go
@@ -0,0 +1,76 @@
+package pipeline
+
+import (
+	"context"
+	"errors"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSimpleHandler_Loop(t *testing.T) {
+	t.Run("ok", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		inch := itemSequence(ctx, 10)
+		outch := make(chan Item, 10)
+
+		f := SimpleHandler(func(ctx context.Context, obj interface{}, err error) (interface{}, error) {
+			return obj, err
+		})
+		err := f.Loop(ctx, inch, outch)
+		require.NoError(t, err)
+
+		close(outch)
+		assert.Len(t, chanToSlice(outch), 10)
+	})
+
+	t.Run("context cancelled", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		mux := sync.Mutex{}
+		mux.Lock()
+		cond := sync.NewCond(&mux)
+
+		go func() {
+			cond.Wait()
+			cancel()
+		}()
+
+		inch := itemSequence(context.Background(), 10)
+		outch := make(chan Item, 10)
+
+		f := SimpleHandler(func(ctx context.Context, obj interface{}, err error) (interface{}, error) {
+			cond.Broadcast()
+			return obj, err
+		})
+		err := f.Loop(ctx, inch, outch)
+		require.Error(t, err)
+		assert.True(t, errors.Is(err, context.Canceled))
+
+		close(outch)
+		assert.Less(t, len(chanToSlice(outch)), 10)
+	})
+}
+
+func itemSequence(ctx context.Context, n int) <-chan Item {
+	ch := make(chan Item)
+	go func() {
+		for obj := range Sequence(ctx, n) {
+			ch <- Wrap(obj, nil)
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+func chanToSlice(ch <-chan Item) (s []interface{}) {
+	for v := range ch {
+		s = append(s, v)
+	}
+	return s
+}
